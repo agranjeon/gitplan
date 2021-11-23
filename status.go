3f38e70a107b35546183ca71dee62d631a45e172
Granjeon Alexandre <agranjeon@clever-age.com>
2021-11-23 13:53:54 +0100

    Forgot to add the file

---
+++ status.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gookit/color"
+	"github.com/jedib0t/go-pretty/v6/table"
+)
+
+func Status() {
+	files, err := ioutil.ReadDir(".gitplan/commits")
+	if err != nil || len(files) == 0 {
+		// Should never happen, but who knows
+		color.Error.Println("I guess you don't have any commit yet huh")
+		return
+	}
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Date", "Branch", "Message"})
+	for _, file := range files {
+		if !strings.Contains(file.Name(), ".info") {
+			continue
+		}
+		content, err := os.ReadFile(".gitplan/commits/" + file.Name())
+		if err != nil {
+			return
+		}
+		fileContent := string(content)
+		s := strings.Split(fileContent, "\n")
+		date, branchName, message := humanDate(s[0]), s[1], s[2]
+
+		t.AppendRow(table.Row{date, branchName, message})
+	}
+	t.Render()
+}
+
+func humanDate(date string) string {
+	humanDate, _ := time.Parse(time.Stamp, date)
+
+	return humanDate.Format("2006-01-02 15:04")
+}
